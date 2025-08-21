bbbb60c464de5ea93fac9d99eca6eb0fa67eabed
Hillary Robert <roberthillary30@gmail.com>
2025-08-21 15:57:33 +0300

    Create a Component to Test ErrorBoundary

--- alx-rick-and-morty-app/pages/index.tsx
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -3,7 +3,8 @@ import { GET_EPISODES } from "@/graphql/queries"
 import { EpisodeProps } from "@/interfaces"
 import EpisodeCard from "@/components/common/EpisodeCard"
 import { useEffect, useState } from "react"
-
+import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorProneComponent from '@/components/ErrorProneComponent';
 
 
 const Home: React.FC = () => {
@@ -66,6 +67,11 @@ const Home: React.FC = () => {
       <footer className="bg-[#4CA1AF] text-white py-4 text-center shadow-md">
         <p>&copy; 2024 Rick and Morty Fan Page</p>
       </footer>
+
+
+      <ErrorBoundary>
+      <ErrorProneComponent />
+    </ErrorBoundary>
     </div>
   )
 }
